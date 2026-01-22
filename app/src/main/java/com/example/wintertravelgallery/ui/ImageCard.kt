269a6620c7f421916cccba0e4aad9e74280c17df
mdidomizio <miriam.didomizio@googlemail.com>
2026-01-22 14:28:00 +0100

    defining details for gallery and imageCard

--- app/src/main/java/com/example/wintertravelgallery/ui/ImageCard.kt
+++ app/src/main/java/com/example/wintertravelgallery/ui/ImageCard.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -51,11 +52,20 @@ fun ImageCard(
                 Box(
                     modifier = Modifier
                         .fillMaxSize()
-                        .background(Color(0xFFE4E7EC)),
+                        .background(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(
+                                    Color(0xFFDAEEFF),
+                                    Color(0xFFEDF7FF)
+                                )
+                            )
+                        ),
                     contentAlignment = Alignment.Center
                 ) {
                     CircularProgressIndicator(
-                        modifier = Modifier.size(40.dp)
+                        modifier = Modifier
+                            .size(40.dp),
+                        color = Color(0xFF5799FC)
                     )
                 }
             },
@@ -64,7 +74,14 @@ fun ImageCard(
                 Box(
                     modifier = Modifier
                         .fillMaxSize()
-                        .background(Color(0xFFFFE9EE)),
+                        .background(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(
+                                    Color(0xFFFFE9EE),
+                                    Color(0xFFFFFBFC)
+                                )
+                            )
+                        ),
                     contentAlignment = Alignment.Center
                 ) {
                     Icon(
