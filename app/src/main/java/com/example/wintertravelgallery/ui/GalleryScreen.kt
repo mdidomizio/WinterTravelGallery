44fc87f4c84374dfe8f813a3acbbf23f82d3432c
mdidomizio <miriam.didomizio@googlemail.com>
2026-01-22 14:16:32 +0100

    defining details for gallery and imageCard

--- app/src/main/java/com/example/wintertravelgallery/ui/GalleryScreen.kt
+++ app/src/main/java/com/example/wintertravelgallery/ui/GalleryScreen.kt
@@ -1,15 +1,26 @@
 package com.example.wintertravelgallery.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -17,7 +28,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -36,17 +49,44 @@ fun GalleryScreen(
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(
-                    text = destination.title,
-                    fontFamily = PlusJakartaSans,
-                    fontWeight = FontWeight.SemiBold
-                )
-                        },
+                title = {
+                    Box(
+                        modifier = Modifier.fillMaxWidth(),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Text(
+                            text = destination.title,
+                            fontFamily = PlusJakartaSans,
+                            fontWeight = FontWeight.SemiBold,
+                            modifier = Modifier.offset(x = (-24).dp)
+                        )
+                    }
+                },
                 navigationIcon = {
-                    IconButton(onClick = { navController.navigateUp() }) {
+                    Button(
+                        onClick = {
+                            navController.popBackStack(
+                                route = "home",
+                                inclusive = false
+                            )
+                        },
+                        modifier = Modifier
+                            .padding(start = 8.dp)
+                            .width(40.dp)
+                            .height(40.dp),
+                        shape = RoundedCornerShape(12.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Transparent,
+                            contentColor = Color(0xFF001221)
+                        ),
+                        border = BorderStroke(1.dp, color = Color(0xFFDCE3E9)),
+                        contentPadding = PaddingValues(0.dp)
+                    ) {
                         Icon(
                             imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = stringResource(R.string.go_back_button_content_description)
+                            contentDescription = stringResource(R.string.go_back_button_content_description),
+                            tint = Color(0xFF001221),
+                            modifier = Modifier.size(24.dp)
                         )
                     }
                 }
@@ -55,13 +95,9 @@ fun GalleryScreen(
     ) { innerPadding ->
         LazyVerticalGrid(
             columns = GridCells.Fixed(2),
-            modifier = modifier
-                .padding(innerPadding)
-                .fillMaxSize(),
-            contentPadding = PaddingValues(4.dp),
-            horizontalArrangement = Arrangement.spacedBy(4.dp),
-            verticalArrangement = Arrangement.spacedBy(4.dp)
-        ){
+            modifier = modifier.padding(innerPadding),
+            contentPadding = PaddingValues(8.dp)
+        ) {
             items(destination.imageUrls) { imageUrl ->
                 ImageCard(
                     imageUrl = imageUrl
--- app/src/main/java/com/example/wintertravelgallery/ui/ImageCard.kt
+++ app/src/main/java/com/example/wintertravelgallery/ui/ImageCard.kt
@@ -1,31 +1,80 @@
 package com.example.wintertravelgallery.ui
 
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import coil3.compose.AsyncImage
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import coil3.compose.SubcomposeAsyncImage
+import coil3.request.CachePolicy
 import coil3.request.ImageRequest
 import coil3.request.crossfade
+import com.example.wintertravelgallery.R
 
 @Composable
 fun ImageCard(
     imageUrl: String,
     modifier: Modifier = Modifier
 ) {
-    Card(modifier = modifier) {
-        AsyncImage(
+    Card(
+        modifier = modifier
+            .padding(horizontal = 4.dp, vertical = 4.dp)
+            .height(220.dp)
+    ) {
+        SubcomposeAsyncImage(
             model = ImageRequest.Builder(context = LocalContext.current)
                 .data(imageUrl)
                 .crossfade(true)
+                .memoryCacheKey(imageUrl)
+                .diskCacheKey(imageUrl)
+                .diskCachePolicy(CachePolicy.ENABLED)
+                .memoryCachePolicy(CachePolicy.ENABLED)
                 .build(),
             contentDescription = null,
             contentScale = ContentScale.Crop,
-            /*error = painterResource(id = R.drawable.ic_broken_image),
-            placeholder = painterResource(id = R.drawable.loading_img),*/
-            modifier = Modifier.aspectRatio(1f)
+            modifier = Modifier.fillMaxSize(),
+            loading = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color(0xFFE4E7EC)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(40.dp)
+                    )
+                }
+            },
+            error = {
+                val color = Color(0xFFF7164B)
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color(0xFFFFE9EE)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Warning,
+                        modifier = Modifier.size(40.dp),
+                        contentDescription = stringResource(id = R.string.image_card_failed_to_load),
+                        tint = color
+                    )
+                }
+            }
         )
     }
 }
